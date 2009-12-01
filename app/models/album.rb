b7e2d032b56d7c10f4760c6a71b1c1b5ea9136e3
Lauri Jutila <lauri@kiskolabs.com>
2009-12-01 15:06:40 -0800

    Album named scope :latest for returning the most recent album.

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,6 +3,8 @@ class Album < ActiveRecord::Base
   belongs_to :artist
   
   named_scope :earliest, :order => :year, :limit => 1
+  named_scope :latest, :order => "year DESC", :limit => 1
+  
   named_scope :quality, lambda { |quality| { :conditions => ['quality = ?', quality] } }  
 
 end
