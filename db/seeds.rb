56d0e49a072102777bdfea077d98dedc30d1c0fd
kristenhazard <kristen@hazardbio.com>
2009-12-01 15:17:34 -0800

    add seed data for albums and artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+
+artist = Artist.create_by_name('Miles Davis')
+
+albums = artist.albums.create
+                       ([ 
+                         {:title => 'Kind of Blue', 
+                          :year => 1959, 
+                          :quality => 'mint'
+                         },
+                         {:title => 'Miles Ahead', 
+                          :year => 1957, 
+                          :quality => 'good'
+                         },
+                         {:title => 'Seven Steps to Heaven', 
+                          :year => 1963, 
+                          :quality => 'poor'
+                         }
+                       ])
+                       
+artist = Artist.create_by_name('Sonny Rollins')
+albums = artist.albums.create
+                       ([ 
+                         {:title => 'Saxophone Colossus', 
+                          :year => 1956, 
+                          :quality => 'mint'
+                         },
+                         {:title => 'Tenor Madness', 
+                          :year => 1957, 
+                          :quality => 'good'
+                         },
+                         {:title => 'Way Out West', 
+                          :year => 1958, 
+                          :quality => 'average'
+                         }
+                       ])
+                       
+artist = Artist.create_by_name('Oscar Peterson')
+albums = artist.albums.create
+                       ([ 
+                         {:title => 'Night Train', 
+                          :year => 1962, 
+                          :quality => 'average'
+                         },
+                         {:title => 'Ben Webster Meets Oscar Peterson', 
+                          :year => 1957, 
+                          :quality => 'mint'
+                         },
+                         {:title => 'The Trio: Live From Chicago', 
+                          :year => 1961, 
+                          :quality => 'poor'
+                         }
+                       ])
