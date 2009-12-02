1d98fda9527074df9e1a49539a83d1776ea38b2e
James Miller <james@jkmillertech.com>
2009-12-02 10:13:43 -0800

    Add generic artist view/controller

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,5 +5,7 @@ class Artist < ActiveRecord::Base
   has_one :first_album,
           :class_name => "Album",
           :order => "year"
+          
+  accepts_nested_attributes_for :albums, :allow_destroy => true, :reject_if => lambda { |a| a.values.all?(&:blank?) }
   
 end
