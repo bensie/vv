661c54861e9bffff6d8785c7039329ba43113398
kristenhazard <kristen@hazardbio.com>
2009-12-02 12:00:09 -0800

    add factories.rb and update tests to reference

--- test/functional/artists_controller_test.rb
+++ test/functional/artists_controller_test.rb
@@ -2,24 +2,6 @@ require 'test_helper'
 
 class ArtistsControllerTest < ActionController::TestCase
   
-  Factory.define :miles, :class => :artist do |artist|
-    artist.name "Miles Davis"
-  end
-  
-  Factory.define :kind_of_blue, :class => :album do |album|
-    album.title "Kind of Blue"
-    album.year  1959
-    album.quality "mint"
-    album.artist { |artist| artist.association(:miles) }
-  end
-
-  Factory.define :miles_ahead, :class => :album do |album|
-    album.title "Miles Ahead"
-    album.year  1957
-    album.quality "mint"
-    album.artist { |artist| artist.association(:miles) }
-  end
-  
   context "get edit form for an Artist" do
     setup do
       @artist = Factory(:miles)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 require 'shoulda'
 require 'factory_girl'
+require File.expand_path(File.dirname(__FILE__) + "/factories.rb")
 
 class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
